7bb40519029fb81b2caaf83d61c9a26e5173f1bf
mikeremerscheid <mikeremerscheid@gmail.com>
2020-03-24 18:15:52 -0400

    Homework Assignment 20200324

--- src/test/java/com/automation/warmup/tablePractice3.java
+++ src/test/java/com/automation/warmup/tablePractice3.java
@@ -1,15 +1,10 @@
 package com.automation.warmup;
 
 import com.automation.utilities.DriverFactory;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
 public class tablePractice3 {
 
@@ -18,8 +13,8 @@ public class tablePractice3 {
     @BeforeMethod
     public void setup(){
         driver = DriverFactory.createDriver("chrome");
-        driver.get("http://practice.cybertekschool.com/dynamic_loading/2");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.get("http://practice.cybertekschool.com/dynamic_loading/2");
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
@@ -27,14 +22,31 @@ public class tablePractice3 {
         driver.quit();
     }
 
-    @Test
-    public void tableTest(){
-        driver.get("http://practice.cybertekschool.com/tables");
-        WebElement value = driver.findElement(By.xpath("//table[1]//tr[2]//td[3]"));
-        String actual = value.getText();
-        String expected = "fbach@yahoo.com";
-        Assert.assertEquals(actual, expected);
-        System.out.println("Expected: " + expected);
-        System.out.println("Actual: " + actual);
-    }
+//    @Test
+//    public void tableTest(){
+//        driver.get("http://practice.cybertekschool.com/tables");
+//        WebElement value = driver.findElement(By.xpath("//table[1]//tr[2]//td[3]"));
+//        String actual = value.getText();
+//        String expected = "fbach@yahoo.com";
+//        Assert.assertEquals(actual, expected);
+//        System.out.println("Expected: " + expected);
+//        System.out.println("Actual: " + actual);
+//    }
+
+//    @Test
+//    public void tableTest2(){
+//        driver.get("http://practice.cybertekschool.com/tables");
+//        List<WebElement> values = driver.findElements(By.xpath("//table[1]//td[last()]"));
+//        System.out.println(driver.findElement(By.xpath("//table[1]//th[last()]")).getText());
+//        for (WebElement value:values){
+//            System.out.println(value.getText());
+//        }
+//    }
+
+//    @Test
+//    public void tableTest4(){
+//        driver.get("http://practice.cybertekschool.com/tables");
+//        System.out.println(driver.findElement(By.xpath("//table[1]//tbody//tr[3]//td[1]")).getText());
+//    }
+
 }
